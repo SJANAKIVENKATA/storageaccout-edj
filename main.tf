cf65d50cff8d0c26c198cc1923373c2a4c05ff0f
SJANAKIVENKATA <93363707+SJANAKIVENKATA@users.noreply.github.com>
2023-12-05 15:27:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 // Modules
 module "storageaccount" {
   source  = "app.terraform.io/edj_demo_org_000/storageaccount/azurerm"
-  version = "1.0.66-beta"
+  version = "1.0.67-beta"
 
   name = "testingedjdemo123"
   resource_group_name = "edj"
